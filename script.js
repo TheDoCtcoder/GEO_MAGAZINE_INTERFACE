ba4562b681c3d17854392dd0a819164d975a5f7c
Toni Deplano <montana.toni@gmail.com>
2023-02-18 22:07:46 +0100

    GEO FR

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function fsurvol()  {
+ console.log("test")
+//  document.getElementById('survol').style.display ="block"
+ document.getElementById('survol').style.display ="flex"
+}
+
+function ferme() {
+    console.log("test2")
+    document.getElementById("survol").style.display = "none"
+}
